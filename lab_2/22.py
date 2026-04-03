e37fe23576a3f917529d1f2163f1dd2aa901a83d
vaback <vaback23@gmail.com>
2026-04-03 22:53:56 +1000

    готово

--- lab_2/22.py
+++ lab_2/22.py
@@ -1,1 +1,29 @@
-#Это файл 22 для лабораторной работы 2
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []  # Здесь будем хранить все готовые комбинации
+        
+        def backtrack(current, left, right):
+            # current: текущий список символов (например, ['(', '('])
+            # left: сколько '(' уже использовали
+            # right: сколько ')' уже использовали
+            
+            # Базовый случай: использовали все скобки
+            if left == right == n:
+                # Превращаем список в строку и добавляем в результат
+                result.append(''.join(current))
+                return
+            
+            # Правило 1: можем добавить '(' если ещё не использовали все
+            if left < n:
+                current.append('(')           # Добавляем
+                backtrack(current, left + 1, right)  # Рекурсивно идём дальше
+                current.pop()                 # Убираем (backtrack!)
+            
+            # Правило 2: можем добавить ')' если закрывающих меньше чем открывающих
+            if right < left:
+                current.append(')')           # Добавляем
+                backtrack(current, left, right + 1)  # Рекурсивно идём дальше
+                current.pop()                 # Убираем (backtrack!)
+        
+        backtrack([], 0, 0)  # Начинаем с пустого списка
+        return result
--- lab_2/622.py
+++ lab_2/622.py
@@ -1,1 +1,76 @@
 #Это файл 622 для лабораторной работы 2
+class MyCircularQueue(object):
+
+    def __init__(self, k):
+        """
+        :type k: int
+        """
+        self.k = k                      # максимальный размер очереди
+        self.queue = [0] * k            # массив фиксированного размера
+        self.front = 0                  # указатель на начало очереди
+        self.rear = -1                  # указатель на конец очереди
+        self.size = 0                   # текущее количество элементов
+
+    def enQueue(self, value):
+        """
+        :type value: int
+        :rtype: bool
+        """
+        if self.isFull():
+            return False
+        
+        # Циклическое перемещение rear
+        self.rear = (self.rear + 1) % self.k
+        self.queue[self.rear] = value
+        self.size += 1
+        return True
+
+    def deQueue(self):
+        """
+        :rtype: bool
+        """
+        if self.isEmpty():
+            return False
+        
+        # Циклическое перемещение front
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+        return True
+
+    def Front(self):
+        """
+        :rtype: int
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.front]
+
+    def Rear(self):
+        """
+        :rtype: int
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.rear]
+
+    def isEmpty(self):
+        """
+        :rtype: bool
+        """
+        return self.size == 0
+
+    def isFull(self):
+        """
+        :rtype: bool
+        """
+        return self.size == self.k
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
